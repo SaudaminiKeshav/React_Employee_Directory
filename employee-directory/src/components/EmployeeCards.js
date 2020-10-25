a344010f0a0a4dad43ed1d292fcc6c4b774c5cbe
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-10-25 11:15:58 -0700

    Populated cards with employee info

--- employee-directory/src/components/EmployeeCards.js
+++ employee-directory/src/components/EmployeeCards.js
@@ -4,13 +4,18 @@ const cardStyle = {
   background: "#fff",
   border: '2px',
   borderRadius: '15px',
-  height: "350px",
+  height: "400px",
   margin: "1rem",
   position: "relative",
-  width: "25%",
+  width: "20%",
   boxShadow: " 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
   display: 'inline-block'
 };
+
+const textStyle = {
+  marginLeft: "12%",
+  textAlign: 'left'
+}
 
 function EmployeeCards(props) {
   return (
@@ -18,7 +23,14 @@ function EmployeeCards(props) {
       <div className="container">
         {props.employees.map(employee => (
           <div className="card" key={employee.id} style={cardStyle}>
-            <div className="card-header"><img src={employee.avatar} style={{width:"35%"}}/></div>
+            <div className="card-header">
+              <br/>
+              <img src={employee.avatar} style={{ width: "35%" }} />
+              <h2>{employee.name}</h2>
+              <p style={textStyle}><b>Title:</b> {employee.title}</p>
+              <p style={textStyle}><b>Salary:</b> {employee.salary}</p>
+              <p style={textStyle}><b>Department:</b> {employee.department}</p>
+            </div>
           </div>
         ))}
       </div>
