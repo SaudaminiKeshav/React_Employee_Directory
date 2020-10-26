c5a1dcde6deeeafcecdfcc3c946d465ea87c68d0
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-10-25 20:12:14 -0700

    Added sorting by employee name feature

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import EmployeeCards from "./components/EmployeeCards"
 import HeaderComponent from "./components/HeaderComponent"
-import employees from "./employees.json";
 import SearchComponent from './components/searchComponent';
 
 function App() {
   return (
-    <div className="App" style={{background:"#fafafa", width:"100%", height:"1500px"}}>
+    <div className="App" style={{ background: "#fafafa", width: "100%"}}>
       <>
         <HeaderComponent />
         <SearchComponent/>
-        <EmployeeCards employees={employees} />
       </>
     </div>
   );
--- employee-directory/src/components/searchComponent.js
+++ employee-directory/src/components/searchComponent.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import employees from "../employees.json";
+import EmployeeCards from "../components/EmployeeCards";
 
-function searchComponent() {
-  return (
-    <div style={{display:"inline-flex", margin:"20px"}}>
-      <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style={{background:"#e1f5fe", marginRight:"20px"}}>
-        Sort by employee name
+function sortByProperty(property){  
+  return function(a,b){  
+     if(a[property] > b[property])  
+        return 1;  
+     else if(a[property] < b[property])  
+        return -1;  
+     return 0;  
+  }  
+}
+
+class searchComponent extends React.Component {
+  state = {
+    employees: []
+  };
+
+   sortBySalary(){
+    let sorted = employees.sort(sortByProperty("salary"))
+    this.setState({
+      [employees]:sorted,
+    })
+    console.log(sorted)
+};
+
+ sortByName(){
+  let sorted = employees.sort(sortByProperty("name"))
+  this.setState({
+    [employees]:sorted,
+  })
+  console.log(sorted)
+};
+
+  render() {
+    return (
+      <>
+        <div style={{ display: "inline-flex", margin: "20px" }}>
+          <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style={{ background: "#e1f5fe", marginRight: "20px" }} onClick={this.sortByName.bind(this)}>
+            Sort by employee name
       </button>
-      <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"  style={{background:"#e1f5fe", marginRight:"20px"}}>
-        Sort by salary
+          <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" style={{ background: "#e1f5fe", marginRight: "20px" }} onClick={this.sortBySalary.bind(this)}>
+            Sort by salary
       </button>
-    
-      <div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-   FILTER BY DEPARTMENT
-  </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenu2" style={{background:"#e1f5fe"}}>
-    <button className="dropdown-item" type="button" style={{fontSize:"120%"}}><b>Executive</b></button><hr/>
-    <button className="dropdown-item" type="button" style={{fontSize:"120%"}}><b>Finance</b></button><hr/>
-    <button className="dropdown-item" type="button" style={{fontSize:"120%"}}><b>Operations</b></button><hr/>
-    <button className="dropdown-item" type="button" style={{fontSize:"120%"}}><b>Information Technology</b></button><hr/>
-    <button className="dropdown-item" type="button" style={{fontSize:"120%"}}><b>Sales</b></button>
-  </div>
-</div>
-<br/><br/>
-    </div>
-  );
+
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              FILTER BY DEPARTMENT
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenu2" style={{ background: "#e1f5fe" }}>
+              <button className="dropdown-item" type="button" style={{ fontSize: "120%" }}><b>Executive</b></button><hr />
+              <button className="dropdown-item" type="button" style={{ fontSize: "120%" }}><b>Finance</b></button><hr />
+              <button className="dropdown-item" type="button" style={{ fontSize: "120%" }}><b>Operations</b></button><hr />
+              <button className="dropdown-item" type="button" style={{ fontSize: "120%" }}><b>Information Technology</b></button><hr />
+              <button className="dropdown-item" type="button" style={{ fontSize: "120%" }}><b>Sales</b></button>
+            </div>
+          </div>
+          <br /><br />
+        </div>
+        <EmployeeCards employees={employees} />
+      </>
+    );
+  }
 }
 
 export default searchComponent;
