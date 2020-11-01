1f955a826aff744fe9f3948c1ab9af8865cd0e82
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-10-31 19:27:59 -0700

    Moved all the search and filter logic to app.js to use useState hook and removed searchComponent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,102 @@
 import './App.css';
+import React, { useState } from 'react';
+
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import HeaderComponent from "./components/HeaderComponent"
-import SearchComponent from './components/searchComponent';
+
+import Employees from "./employees.json";
+import EmployeeCards from "./components/EmployeeCards";
+
+function sortByProperty(property) {
+  return function (a, b) {
+    if (a[property] > b[property])
+      return 1;
+    else if (a[property] < b[property])
+      return -1;
+    return 0;
+  }
+}
 
 function App() {
+  const [employees, setEmployees] = useState(Employees);
+  const [filterDept, setFilterDept] = useState('');
+
+  function sortBySalary() {
+    let sorted = [...employees].sort(sortByProperty("salary"))
+    setEmployees(sorted);
+    console.log(sorted)
+  };
+
+  function sortByName() {
+    let sorted = [...employees].sort(sortByProperty("name"))
+    setEmployees(sorted);
+    console.log(sorted)
+  };
+
+  function filterByDept(event) {
+    if (event.target.value != null && event.target.value != "") {
+      let filterEmployees = employees.filter(employee => {
+        return employee.department === event.target.value;
+      });
+      setEmployees(filterEmployees);
+      console.log(employees)
+    }
+  }
+
+  function resetArray() {
+    setEmployees(Employees);
+  };
+
   return (
     <div className="App" style={{ background: "#fafafa", width: "100%"}}>
       <>
         <HeaderComponent />
-        <SearchComponent/>
+        <>
+        <div style={{ display: "inline-flex", margin: "20px" }}>
+          <button
+            className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+            style={{ background: "#e1f5fe", marginRight: "20px" }}
+            onClick={sortByName.bind(this)}>
+            Sort by employee name
+      </button>
+
+          <button
+            className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+            style={{ background: "#e1f5fe", marginRight: "20px" }}
+            onClick={sortBySalary.bind(this)}>
+            Sort by salary
+      </button>
+          <hr />
+
+          <FormControl style={{ width: 200, height: "100%" }}>
+            <InputLabel id="filter-dept-label">Filter by Department</InputLabel>
+            <Select
+              labelId="filter-dept-label"
+              id="filter-dept-select"
+              onChange={filterByDept.bind(this)}
+            >
+              <MenuItem value="Executive">Executive</MenuItem>
+              <MenuItem value="Finance">Finance</MenuItem>
+              <MenuItem value="Operations">Operations</MenuItem>
+              <MenuItem value="Information Technology">Information Technology</MenuItem>
+              <MenuItem value="Sales">Sales</MenuItem>
+            </Select>
+          </FormControl>
+          <ButtonGroup size="small" color="primary" aria-label="outlined primary button group">
+          <Button onClick={resetArray} >
+            Reset Employees
+            </Button>
+        </ButtonGroup>
+          <br /><br />
+
+        </div>
+        <EmployeeCards employees={employees} />
+      </>
       </>
     </div>
   );
--- src/components/searchComponent.js
+++ src/components/searchComponent.js
@@ -1,97 +1,92 @@
-import React from 'react';
-import employees from "../employees.json";
-import EmployeeCards from "../components/EmployeeCards";
+// import React from 'react';
+// import employees from "../employees.json";
+// import EmployeeCards from "../components/EmployeeCards";
 
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+// function sortByProperty(property) {
+//   return function (a, b) {
+//     if (a[property] > b[property])
+//       return 1;
+//     else if (a[property] < b[property])
+//       return -1;
+//     return 0;
+//   }
+// }
 
-function sortByProperty(property) {
-  return function (a, b) {
-    if (a[property] > b[property])
-      return 1;
-    else if (a[property] < b[property])
-      return -1;
-    return 0;
-  }
-}
+// class searchComponent extends React.Component {
+//   state = {
+//     employees: []
+//   };
 
-class searchComponent extends React.Component {
-  state = {
-    employees: []
-  };
+//   sortBySalary() {
+//     let sorted = employees.sort(sortByProperty("salary"))
+//     this.setState({
+//       [employees]: sorted,
+//     })
+//     console.log(sorted)
+//   };
 
-  sortBySalary() {
-    let sorted = employees.sort(sortByProperty("salary"))
-    this.setState({
-      [employees]: sorted,
-    })
-    console.log(sorted)
-  };
+//   sortByName() {
+//     let sorted = employees.sort(sortByProperty("name"))
+//     this.setState({
+//       [employees]: sorted,
+//     })
+//     console.log(sorted)
+//   };
 
-  sortByName() {
-    let sorted = employees.sort(sortByProperty("name"))
-    this.setState({
-      [employees]: sorted,
-    })
-    console.log(sorted)
-  };
-
-  filterByDept(event) {
-    if (event.target.value != null && event.target.value != "") {
-      let filterEmployees = employees.filter(employee => {
-        return employee.department === event.target.value;
-      });
-      this.setState({
-        [employees]: filterEmployees
-      })
-      console.log(employees)
-    }
-  }
+//   // filterByDept(event) {
+//   //   if (event.target.value != null && event.target.value != "") {
+//   //     let filterEmployees = employees.filter(employee => {
+//   //       return employee.department === event.target.value;
+//   //     });
+//   //     this.setState({
+//   //       [employees]: filterEmployees
+//   //     })
+//   //     console.log(employees)
+//   //   }
+//   // }
 
 
-  render() {
-    return (
-      <>
-        <div style={{ display: "inline-flex", margin: "20px" }}>
-          <button
-            className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-            style={{ background: "#e1f5fe", marginRight: "20px" }}
-            onClick={this.sortByName.bind(this)}>
-            Sort by employee name
-      </button>
+//   render() {
+//     return (
+//       <>
+//         <div style={{ display: "inline-flex", margin: "20px" }}>
+//           <button
+//             className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+//             style={{ background: "#e1f5fe", marginRight: "20px" }}
+//             onClick={this.sortByName.bind(this)}>
+//             Sort by employee name
+//       </button>
 
-          <button
-            className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
-            style={{ background: "#e1f5fe", marginRight: "20px" }}
-            onClick={this.sortBySalary.bind(this)}>
-            Sort by salary
-      </button>
-          <hr />
+//           <button
+//             className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+//             style={{ background: "#e1f5fe", marginRight: "20px" }}
+//             onClick={this.sortBySalary.bind(this)}>
+//             Sort by salary
+//       </button>
+//           <hr />
 
-          <FormControl style={{ width: 200, height: "100%" }}>
-            <InputLabel id="filter-dept-label">Filter by Department</InputLabel>
-            <Select
-              labelId="filter-dept-label"
-              id="filter-dept-select"
-              onChange={this.filterByDept.bind(this)}
-            >
-              <MenuItem value="Executive">Executive</MenuItem>
-              <MenuItem value="Finance">Finance</MenuItem>
-              <MenuItem value="Operations">Operations</MenuItem>
-              <MenuItem value="Information Technology">Information Technology</MenuItem>
-              <MenuItem value="Sales">Sales</MenuItem>
-            </Select>
-          </FormControl>
+//           <FormControl style={{ width: 200, height: "100%" }}>
+//             <InputLabel id="filter-dept-label">Filter by Department</InputLabel>
+//             <Select
+//               labelId="filter-dept-label"
+//               id="filter-dept-select"
+//               onChange={this.filterByDept.bind(this)}
+//             >
+//               <MenuItem value="Executive">Executive</MenuItem>
+//               <MenuItem value="Finance">Finance</MenuItem>
+//               <MenuItem value="Operations">Operations</MenuItem>
+//               <MenuItem value="Information Technology">Information Technology</MenuItem>
+//               <MenuItem value="Sales">Sales</MenuItem>
+//             </Select>
+//           </FormControl>
 
-          <br /><br />
+//           <br /><br />
 
-        </div>
-        <EmployeeCards employees={employees} />
-      </>
-    );
-  }
-}
+//         </div>
+//         <EmployeeCards employees={employees} />
+//       </>
+//     );
+//   }
+// }
 
-export default searchComponent;
+// export default searchComponent;
